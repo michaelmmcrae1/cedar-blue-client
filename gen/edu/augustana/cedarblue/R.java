aaff0721e2c784485289ccc6c8d082f8b31d94b1
unknown <evanshaw11@OLINLAB204-01.augustana.edu>
2014-04-23 16:03:32 -0500

    Clean up TopicBoard XML, add clickListener, global variables

--- gen/edu/augustana/cedarblue/R.java
+++ gen/edu/augustana/cedarblue/R.java
@@ -24,14 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f08000b;
         public static final int button1=0x7f080001;
         public static final int button2=0x7f080000;
         public static final int button3=0x7f080002;
         public static final int button4=0x7f080003;
         public static final int button5=0x7f080004;
+        public static final int editText1=0x7f080006;
         public static final int textView1=0x7f080005;
-        public static final int textView2=0x7f080006;
+        public static final int textView2=0x7f080007;
+        public static final int textView3=0x7f080008;
+        public static final int textView4=0x7f080009;
+        public static final int textView5=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -49,6 +53,7 @@ public final class R {
         public static final int other_button=0x7f050007;
         public static final int sport_button=0x7f050005;
         public static final int study_button=0x7f050008;
+        public static final int submit_button=0x7f050009;
         public static final int welcome=0x7f050003;
     }
     public static final class style {
--- src/edu/augustana/cedarblue/TopicBoardActivity.java
+++ src/edu/augustana/cedarblue/TopicBoardActivity.java
@@ -25,10 +25,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class TopicBoardActivity extends Activity {
+public class TopicBoardActivity extends Activity implements View.OnClickListener {
+	public JSONArray jArray;
 	public String topicString;
+	public TextView text1;
+	public TextView text2;
+	public TextView text3;
+	public TextView text4;
+	public TextView text5;
+	public Button submitButton;
 
 	/** Called when the activity is first created. */
 	@SuppressLint("NewApi")
@@ -38,20 +48,32 @@ public class TopicBoardActivity extends Activity {
 	    
 	    /*
 		 * Rather than create an AsyncTask to connect over HTTP, I just want to do it here
-		 * as a demo. This is a quick hack that allows the main activity to use HTTP
+		 * as a demo. This is a quick hack that allows the UI thread to use HTTP
 		 */
 	    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 	    StrictMode.setThreadPolicy(policy); 
 	
-	    // TODO Auto-generated method stub
+	    // Get the most recent posts for this topic, store in jArray
 	    Bundle nameHashMap = this.getIntent().getExtras();
         topicString = nameHashMap.getString("topicString");
+        
+        submitButton = (Button) findViewById(R.id.button1);
+        text1 = (TextView) findViewById(R.id.textView1);
+        text2 = (TextView) findViewById(R.id.textView2);
+        text3 = (TextView) findViewById(R.id.textView3);
+        text4 = (TextView) findViewById(R.id.textView4);
+        text5 = (TextView) findViewById(R.id.textView5);
+        
+        submitButton.setOnClickListener(this);
+        
         try {
 			getPosts();
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+        
+        
 	}
 	
 	
@@ -59,23 +81,27 @@ public class TopicBoardActivity extends Activity {
 	public void getPosts() throws JSONException {
 		// Create a new HttpClient and Post Header
 	    HttpClient httpclient = new DefaultHttpClient();
-	    HttpPost httppost = new HttpPost("http://10.100.31.21/cblue/topicController.php");
+	    HttpPost httppost = new HttpPost("http://10.100.31.21/cblue/getPostsFromDB.php");
 	    Toast.makeText(getApplicationContext(), "Topic: " + topicString, Toast.LENGTH_LONG).show();
 	    try {
-	        // Add your data
+	        // Bind values with List
 	        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
 	        nameValuePairs.add(new BasicNameValuePair("topic", topicString));
+	        
+	        // Encode values on URL entity
 	        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 	        Log.e("log_tag", "In try");
-	        // Execute HTTP Post Request
+	        
+	        // Execute HTTP Post, capture response
 	        HttpResponse response = httpclient.execute(httppost);
 	        Log.e("log_tag", "Made it past http.execute");
+	        
 	        // Begin to retrieve JSON data from server
 	        HttpEntity entity = response.getEntity();
 	        if (entity != null) {
 	            InputStream is = entity.getContent();
 	            BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"));
-	            
+	            // Use StringBuilder sb to acquire entire response as a String
 	            StringBuilder sb = new StringBuilder();
 				String line = null;
 				while ((line = reader.readLine()) != null) {
@@ -85,12 +111,17 @@ public class TopicBoardActivity extends Activity {
 				
 				String result = sb.toString();
 				Log.e("log_tag", "Result: " + result);
-	            JSONArray jArray = new JSONArray(result);
+				
+				/*
+				 *  Instantiate JSONArray with our String
+				 *  We now have the data in a JSONArray. We will use this JSONArray to populate TextViews
+				 *  with messages.
+				 */
+	            jArray = new JSONArray(result);
 	            
-	            Log.e("log_tag", "Just before jArray.toString()");
-				Log.e("log_tag", "JSON: " + jArray.toString());
+				Log.e("log_tag", "JSONArray: " + jArray.toString());
 				//Toast.makeText(getApplicationContext(), "" + jObject.getString("message"), Toast.LENGTH_LONG).show();
-				Log.e("log_tag", "JSON at 1: " + jArray.get(1));
+				Log.e("log_tag", "JSON at 0: " + jArray.get(0));
 	        }
 	        Toast.makeText(getApplicationContext(), "Entity is null", Toast.LENGTH_LONG).show();
 	    } catch (ClientProtocolException e) {
@@ -99,9 +130,25 @@ public class TopicBoardActivity extends Activity {
 	    } catch (IOException e) {
 	        // TODO Auto-generated catch block
 	    	e.printStackTrace();
+	    } catch (JSONException e) {
+	    	e.printStackTrace();
 	    }
 	}
 	
+	public void populateTextViews() {
+		
+	}
+	
+	
+	/*
+	 * Take user's text from editText, and load it into correct topic table on server.
+	 * Then, reload current topic and redisplay posts, including the user's recent submission
+	 */
+	@Override
+	public void onClick(View arg0) {
+		Toast.makeText(getApplicationContext(), "Clicked: " + arg0.toString(), Toast.LENGTH_LONG).show();
+		
+	}
 	
 
 }
